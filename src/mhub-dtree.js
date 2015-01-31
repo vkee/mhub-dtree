07639e01640d58413fc8c47c7ea4bc60764110d9
ijonesalvarez <doubleisrael@gmail.com>
2015-01-31 01:39:18 -0800

    Implemented getNextLeader and getNode... hopefully

--- src/mhub-dtree.js
+++ src/mhub-dtree.js
@@ -1,6 +1,7 @@
 var secret = '40e2d09610aa1bead4583720a2377e24f9fe2b1844b1299ceaff0c2d1186d2e2';
 
 var queue = [];
+var nodeCounter = 0;
 
 var mocks = {
     "T1Leader": {
@@ -31,7 +32,9 @@ var processLeader = function () {
 
 // @todo: unstub
 var getNextLeader = function() {
-    return mocks.T1Leader;
+    //return mocks.T1Leader;
+	// This works as an actual queue but apparently can be slow
+	return queue.shift();
 }
 
 // @todo: unstub
@@ -42,8 +45,25 @@ var getGroupIdsLeadBy = function(leader) {
 // @todo: unstub
 var getNode(person) {
     //If person hasn't been encountered yet, create new node and return
-    //Else, return existing node
-    return {};
+	// This should catch if there is no node property yet in person. It should work for both
+	// undefined cases and null cases
+	if (person.node == null)
+	{
+		//Push the person into the queue
+		queue.push(person);
+		// Using the setNode for D3
+		setNode(nodeCounter, {label: person.name});
+		// Add a node property to the person 
+		person.push("node": nodeCounter); 
+		// Increase the global node counter so we have unique node numbers for everyone
+		// Israel: I think that we could use the person's id for setNode instead of a 
+		// global node counter but I think this is the way you wanted it formatted
+		nodeCounter++;
+		return person.node;
+    }
+	//Else, return existing node
+	else
+		return person.node;
 }
 
 // @todo: unstub
