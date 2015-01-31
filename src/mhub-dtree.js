246ac12c109058688a3adcb8e002c7c696eb78cd
nateemerson <emerson.nate@gmail.com>
2015-01-31 14:24:54 -0800

    partial peopleLeadBy

--- src/mhub-dtree.js
+++ src/mhub-dtree.js
@@ -1,29 +1,42 @@
-var secret = '40e2d09610aa1bead4583720a2377e24f9fe2b1844b1299ceaff0c2d1186d2e2';
-var base_url = 'https://stage.missionhub.com/apis/v3/';
-var queue = [];
-var discipleship = new Graph();
-var nodesCreated = [];
+var secret = '40e2d09610aa1bead4583720a2377e24f9fe2b1844b1299ceaff0c2d1186d2e2',
+    base_url = 'https://stage.missionhub.com/apis/v3/',
+    queue = [],
+    discipleship = new Graph(),
+    nodesCreated = [],
 
-var mocks = {
-    "T1Leader": {
-        "id": 12,
-        "name": "Drew Aufhammer"
-    },
-    "T2Leader": {
-        "id": 45,
-        "name": "Nate Emerson"
-    },
-    "T1LeaderGroups": [1, 3, 5]
-};
+    mocks = {
+        "T1Leader": {
+            "id": 12,
+            "name": "Drew Aufhammer"
+        },
+        "T2Leader": {
+            "id": 45,
+            "name": "Nate Emerson"
+        },
+        "T1LeaderGroups": [1, 3, 5]
+    };
+
+var makeNode = function(person) {
+    // Checks if a node has been created for a given person yet
+    if (nodesCreated.indexOf(person.id) == -1)
+    {
+        //Push the person into the queue
+        queue.push(person);
+        //Push the id into created notes
+        nodesCreated.push(person.id);
+        // Using the addNode for D3
+        discipleship.addNode(person.id, person.name);
+    }
+}
+
+var makeEdge = function(node1, node2) {
+    discipleship.addEdge(node1, node2);
+}
 
 // Main function of algorithm that processes one item from queue.
-
 var processLeader = function () {
     var leader = getNextLeader();
-    var groupIds = getGroupIdsLeadBy(leader);
-    //var leaderNode = getNode(leader);
-    $.each(groupIds, function(index, groupId) {
-        var people = getMembersForGroupId(groupId);
+    peopleLeadBy(leader, function(people) {
         $.each(people, function(index, person) {
             makeNode(person);
             makeEdge(leader.id, person.id);
@@ -31,37 +44,27 @@ var processLeader = function () {
     });
 }
 
-// @todo: unstub
 var getNextLeader = function() {
-    //return mocks.T1Leader;
-    // This works as an actual queue but apparently can be slow
     return queue.shift();
 }
 
 // @todo: unstub
-var getGroupIdsLeadBy = function(leader) {
-    return mocks.T1LeaderGroups;
-}
+var peopleLeadBy = function(leader, successCallback) {
+    //Query mission hub for the list of people that are leaders but not a member
+    //of any group themselves.
+    queryMissionHub('group_memberships', {'filters[leader_id]': leader.id}, function(json) {
+        //Parse through the data from Missionhub and just take ID and name
+        $.each(json.group_memberships, function(index, membership) {
 
-// @todo: unstub
-var makeNode = function(person) {
-    //If person hasn't been encountered yet, create new node and return
-    // This should catch if there is no node property yet in person. It should work for both
-    // undefined cases and null cases
-    if (nodesCreated.indexOf(person.id) == -1))
-    {
-        //Push the person into the queue
-        queue.push(person);
-		//Push the id into created notes
-		nodesCreated.push(person.id);
-        // Using the addNode for D3
-        discipleship.addNode(person.id, person.name);
-    }
-}
-
-// @todo: unstub
-var makeEdge = function(node1, node2) {
-	discipleship.addEdge(node1, node2);
+        })
+        successCallback(json);
+        // $.each(json.people, function(index, person) {
+        //     queue.push({
+        //         id: person.id,
+        //         name: person.first_name+' '+person.last_name
+        //     });
+        // });
+    });
 }
 
 // Gets the root leaders from Missionhub and begins processing the queue
@@ -79,9 +82,7 @@ var getRootLeaders = function() {
         });
 
         // Begin processing the queue.
-        // @todo: unstub
-        //processLeader();
-        console.log(queue);
+        processLeader();
     });
 }
 
