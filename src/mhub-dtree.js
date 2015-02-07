e872ecfa7d07367ef5811889dcdef4d185b734c4
Vincent Kee <vkee@mit.edu>
2015-02-07 12:33:43 -0500

    added hacky loop and cleaned up code

--- src/mhub-dtree.js
+++ src/mhub-dtree.js
@@ -1,7 +1,6 @@
 var secret = '40e2d09610aa1bead4583720a2377e24f9fe2b1844b1299ceaff0c2d1186d2e2',
     base_url = 'https://stage.missionhub.com/apis/v3/',
     rootLeaders = [],
-    queue = [],
     discipleship = new Graph(),
     nodesCreated = [];
 
@@ -26,31 +25,17 @@ var makeEdge = function(node1, node2) {
     discipleship.addEdge(node1, node2);
 }
 
-// Main function of algorithm that processes one item from queue.
-var processLeader = function () {
+// Generate a tree of the people led by the leader at the provided index
+var peopleLedBy = function(index) {
+    leader = rootLeaders[index];
 
-    // Iterate through leaders using the callback hacked together loop
+    // Root of the tree
+    makeNode(leader);
 
-    console.log('queue');
-    console.log(queue);
-    var leader = getNextLeader();
-    console.log('leader');
-    console.log(leader);
-    // console.log('queue');
-    // console.log(queue);
-    peopleLedBy(leader, // callback that will continue to iterate through the loop)
-}
-
-var getNextLeader = function() {
-    return rootLeaders.shift();
-}
-
-// @todo: unstub
-var peopleLedBy = function(leader, successCallback) {
     // Getting all the members led by the leader
     queryMissionHub('group_memberships', {'filters[leader_id]': leader.id}, function(members) {
 
-        // for each memeber, need to make another query to get the actual name and id of the person
+        // for each member, need to make another query to get the actual name and id of the person
         // once receive that in the callback add node and edge to tree
 
         // 
@@ -59,30 +44,23 @@ var peopleLedBy = function(leader, successCallback) {
         //     // makeNode(person);
         //     // makeEdge(leader.id, person.id);
         // });
-    });
 
-        //Parse through the data from Missionhub and just take ID and name
-        // $.each(json.group_memberships, function(index, membership) {
-            
-        // });
-    successCallback(json);
-        // $.each(json.people, function(index, person) {
-        //     queue.push({
-        //         id: person.id,
-        //         name: person.first_name+' '+person.last_name
-        //     });
-        // });
-    // });
+        // Once done building all trees, render the graph
+        if (index == (rootLeaders.length - 1)){
+            discipleship.renderGraph();
+        } else {
+            // Otherwise keep building the tree with the next root leader
+            peopleLedBy(index + 1);
+        }
+    });
 }
 
-// Gets the root leaders from Missionhub and begins processing the queue
+// Gets the root leaders from Missionhub
 var getRootLeaders = function() {
     // Query mission hub for the list of people that are leaders but not a member
     // of any group themselves.
     queryMissionHub('people', {'filters[group_involvement_id]': 'none', 'filters[group_role]': 'leader'}, function(json) {
-        console.log('orig');
-        console.log(json);
-        //Parse through the data from Missionhub and just take ID and name
+        // Parse through the data from Missionhub and just take ID and name
         $.each(json.people, function(index, person) {
             rootLeaders.push({
                 id: person.id,
@@ -90,8 +68,7 @@ var getRootLeaders = function() {
             });
         });
 
-        // Begin processing the root leaders
-        processLeader();
+        peopleLedBy(0);
     });
 }
 
@@ -102,7 +79,7 @@ var queryMissionHub = function(endpoint, options, successCallback)
     var url = base_url+endpoint+'?secret='+secret;
 
     $.each(options, function(key, value) {
-        url += '&'+encodeURIComponent(key)+'='+encodeURIComponent(value);
+        url += '&' + encodeURIComponent(key) + '=' + encodeURIComponent(value);
     });
 
     $.ajax({
@@ -115,13 +92,7 @@ var queryMissionHub = function(endpoint, options, successCallback)
     });
 }
 
-var init = function() {
-    getRootLeaders();
-}
-
-var finish = function() {
-    discipleship.renderGraph();
-}
+getRootLeaders();
 
 //leaders
 //https://stage.missionhub.com/apis/v3/people?secret=40e2d09610aa1bead4583720a2377e24f9fe2b1844b1299ceaff0c2d1186d2e2&filters%5Bgroup_role%5D=leader
